ba898a895b40b614303c94d9efd5133be05b1901
James Ousby <jousby@gmail.com>
2017-12-14 17:45:43 +0800

    --wip-- [skip ci]

--- src/main/scala/com/example/VizceralFeed.scala
+++ src/main/scala/com/example/VizceralFeed.scala
@@ -1,6 +1,7 @@
 package com.example
 
-import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+import java.time.{Clock, LocalDateTime, ZonedDateTime}
 
 import akka.actor.{Actor, ActorLogging, ActorRef, ActorSystem, Props}
 import com.amazonaws.auth.profile.ProfileCredentialsProvider
@@ -9,18 +10,20 @@ import io.circe._
 import io.circe.generic.auto._
 import io.circe.parser._
 import io.circe.syntax._
+import scala.concurrent.duration._
 
 object VizceralFeedActor {
   def props(profileName: String): Props = {
     val s3 = AmazonS3ClientBuilder
       .standard()
       .withCredentials(new ProfileCredentialsProvider(profileName))
+      .withRegion("ap-southeast-1")
       .build()
     props(s3)
   }
   def props(s3: AmazonS3): Props = Props(new VizceralFeedActor(s3))
 
-  final case class ProcessFeedInterval(interval: Int)
+  final case object ProcessFeedInterval
 
   def buildGraph(): String = {
     // apSoutheast1 graph
@@ -51,16 +54,26 @@ object VizceralFeedActor {
 
 class VizceralFeedActor(s3: AmazonS3) extends Actor with ActorLogging {
   import VizceralFeedActor._
+  import context.dispatcher
+
+  val bucketName = "vizceraltemplates"
 
   def receive() = {
-    case ProcessFeedInterval(interval) => {
+    case ProcessFeedInterval => {
+      val now = LocalDateTime.now(Clock.systemUTC())
+
       // load from ES
+
       // build graph
       val newGraph = buildGraph()
-      println(s"New graph has been built: $newGraph")
 
       // push to S3
-      // schedule next
+      val dateString = now.format(DateTimeFormatter.ISO_DATE_TIME)
+      s3.putObject(bucketName, "vizgraph.json.latest", newGraph)
+      s3.putObject(bucketName, s"vizgraph.json.$dateString", newGraph)
+
+      // Schedule another interval check in 10 second
+      context.system.scheduler.scheduleOnce(10 second, self, ProcessFeedInterval)
     }
   }
 }
@@ -72,5 +85,5 @@ object VizceralFeed extends App {
 
   val viz: ActorRef = system.actorOf(VizceralFeedActor.props("devman"), "vizActor")
 
-  viz ! ProcessFeedInterval(1)
+  viz ! ProcessFeedInterval
 }
